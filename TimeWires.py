d1f5d5371d0a57dc9a8534721b1f3c0e30407b90
monica-gatti <gatti_monica@libero.it>
2022-12-14 18:27:32 +0100

    article body added to json

--- TimeWires.py
+++ TimeWires.py
@@ -10,15 +10,17 @@ data = requests.get(apiUrl).text
 
 data = json.loads(data)
 
-url,page,title,updated_Date, = "","", "", ""
 for result in data["results"][10:11]:
     url = result["url"]
-    print(url)
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    #req.add_header('Accept-Language', 'fr-FR')
     page = urlopen(req)
     soup = bs(page, 'html.parser')
-    for t in soup.findAll("p", attrs={"class":"css-at9mc1 evys1bk0"}):#[0]#.findAll("td",attrs={"titleColumn"})
-        print(t.text)
+    body =""
+    for t in soup.findAll("p", attrs={"class":"css-at9mc1 evys1bk0"}):
+        body = body + t.text
+    result["body"] = body
+
+with open("allnyt.json", "w") as jsonFile:
+    json.dump(data, jsonFile)
 
 
