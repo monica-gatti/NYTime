18228ee21964691f94a0abe517666ac58823de11
monica-gatti <gatti_monica@libero.it>
2022-12-14 17:20:09 +0100

    got body article

--- TimeWires.py
+++ TimeWires.py
@@ -11,11 +11,18 @@ data = requests.get(apiUrl).text
 data = json.loads(data)
 
 url,page,title,updated_Date, = "","", "", ""
-for result in data["results"]:
+for result in data["results"][10:11]:
     url = result["url"]
-    req = Request(url)
+    print(url)
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     #req.add_header('Accept-Language', 'fr-FR')
     page = urlopen(req)
     soup = bs(page, 'html.parser')
+    for t in soup.findAll("p", attrs={"class":"css-at9mc1 evys1bk0"}):#[0]#.findAll("td",attrs={"titleColumn"})
+        print(t.text)
 
-    print(url)
+    #body = soup.find("p", class_="css-at9mc1 evys1bk0").text
+
+    #for e in soup.select('div.css-at9mc1'):
+    #    print(e.text)
+
