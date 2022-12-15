8c58859af6ef3042bf0fb37c83b5b559c68d6e0b
YassinCHAKOURI <chakouri.yassine@gmail.com>
2022-12-15 12:06:35 +0100

    ajout Article_Search_API.py

---
+++ Article_Search_API.py
@@ -0,0 +1,8 @@
+import json
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen, Request
+import requests
+from pprint import pprint
+import yaml
+from utils import getNYTUrl, getHeaders
+import ast
