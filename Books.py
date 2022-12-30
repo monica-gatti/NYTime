d6ea33b57b6c844ed1e6a5944730dc386b0f62d6
Raouf-Yahiaoui <abderraoufraouf@gmail.com>
2022-12-30 11:43:05 +0100

    modification sur Books

--- Books.py
+++ Books.py
@@ -1,65 +1,37 @@
-from utils import getNYTUrl, getUserAgent
-from urllib.request import urlopen, Request
-from bs4 import BeautifulSoup as bs
-from datetime import datetime
-from pprint import pprint
-import pandas as pd
-import requests
+
 import json
-import yaml
+from urllib.request import urlopen, Request
+from utils import getNYTUrl,  getStringCurrentDate, ingestForES
+from bs4 import BeautifulSoup as bs
+import requests
+from pprint import pprint
 import ast
+import sqlite3 
+import logging
 
+logging.basicConfig(filename="./output/logBooks.log", level=logging.INFO)
 
-apiUrl = getNYTUrl(context_='BOOKS_CONTEXT')
-
-data = requests.get(apiUrl).text
+BOOKS_CONTEXT = getNYTUrl(context_="BOOKS_CONTEXT")
+data = requests.get(BOOKS_CONTEXT).text
 data = json.loads(data)
 
-best_sellers_history = []
-
-for index in range(0,len(data['results']['lists'])):
-    for book in range(0,len(data['results']['lists'][index]['books'])):
-        title = data['results']['lists'][index]['books'][book]['author']
-        author = data['results']['lists'][index]['books'][book]['author']
-        created_date = data['results']['lists'][index]['books'][book]['created_date']
-        description = data['results']['lists'][index]['books'][book]['description']
-        primary_isbn10 = data['results']['lists'][index]['books'][book]['primary_isbn10']
-        updated_date = data['results']['lists'][index]['books'][book]['updated_date']
-        contributor = data['results']['lists'][index]['books'][book]['contributor']
-        publisher = data['results']['lists'][index]['books'][book]['publisher']
-        rank = data['results']['lists'][index]['books'][book]['rank']
-        amazon_product_url = data['results']['lists'][index]['books'][book]['amazon_product_url']
-
-
-        dictionnaire = {'title': title,
-                        'author': author,
-                        'created_date':created_date,
-                        'description' :description,
-                        'primary_isbn10' : primary_isbn10,
-                        'updated_date' : updated_date,
-                        'contributor' : contributor,
-                        'publisher' : publisher,
-                        'rank' : rank,
-                        'amazon_product_url' : amazon_product_url
-                    }
-        best_sellers_history.append(dictionnaire)
-
-#pprint(best_sellers_history[0:2])      
-
-#Obtaining amazon_urls from best_sellers_history to do web scraping in order to get the prices of the books that are associated with each URL
-
-for url in range(0,len(best_sellers_history)):
-    amazon_product_url = best_sellers_history[url]['amazon_product_url']
-    #pprint(amazon_product_url)
-    req = Request(amazon_product_url, headers=ast.literal_eval("{'User-Agent': 'Mozilla/5.0'}"))
-    page = urlopen(req)
-    soup = bs(page, 'html.parser')
-    #print(soup) 
-    print(soup.find('span',{'class':'a-size-base a-color-price a-color-price'}).get_text(strip=True))
-
-now = datetime.now()
-dt_string = now.strftime("%d-%m-%Y_%H%M%S")
-
-filename = "./output/" + dt_string + "_books.json" 
-with open(filename, "w") as jsonFile:
-    json.dump(data, jsonFile)
+for lists in data['results']['lists']:
+    for book in lists['books']:
+        con = sqlite3.connect('books.db')
+        cur = con.cursor()
+        try:  
+            cur.execute("INSERT INTO books_full_overview VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+            (book['title'], book['author'], book['contributor'], book['publisher'], book['rank'], book['primary_isbn10'], book['primary_isbn13'],book['updated_date'], book['created_date'],book[' amazon_product_url']))
+            con.commit()     
+        except  sqlite3.IntegrityError as err:
+            print(f"Integritya error {err=}, {type(err)=}")
+        try:
+            ingestForES(book['title'], book['author'], book['contributor'], book['publisher'],book[' descriptionbook'], book['rank'], book['primary_isbn10'], book['primary_isbn13'],book['updated_date'], book['created_date'],book[' amazon_product_url'], book['weeks_on_list'], book[' buy_links'])
+        except Exception as err:
+            print(f"Unexpected {err=}, {type(err)=}")
+    con.close()
+        
+    filename = "./output/" + getStringCurrentDate() + "_books_full_overview.json" 
+    with open(filename, "w") as jsonFile:
+        json.dump(data, jsonFile)    
+        
