ec982468d61e54c33b18d53943379bed94f53fa4
Raouf-Yahiaoui <abderraoufraouf@gmail.com>
2022-12-30 11:51:58 +0100

    ajout sqlit.con for books.db

--- db-model/sqlite-script.py
+++ db-model/sqlite-script.py
@@ -1,5 +1,9 @@
 import sqlite3
+
 con = sqlite3.connect("nytimes.db")
+
+con = sqlite3.connect('books.db')
+
 
 cur = con.cursor()
 
@@ -9,6 +13,16 @@ con.commit()
 cur.execute("CREATE TABLE authors(slug_name, fullname, FOREIGN KEY(slug_name) REFERENCES articles(slug_name))")
 con.commit()
 
+cur.execute('''CREATE TABLE books_full_overview(id INTEGER PRIMARY KEY, title TEXT, author TEXT, publisher TEXT, contributor TEXT, rank INTEGER, primary_isbn13 INTEGER, primary_isbn10 INTEGER, updated_date DATE, created_date DATE, amazon_product_url TEXT)''')
+con.commit()
+
+cur.execute('''CREATE TABLE book_prices(id INTEGER PRIMARY KEY, url, price)''')
+con.commit()
+
 res = cur.execute("SELECT name FROM sqlite_master")
 print(res.fetchall())
 con.close()
+
+
+
+
