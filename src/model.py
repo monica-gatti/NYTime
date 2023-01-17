72a21a7ca8515256ab1d3ff7d11cf450f7606efe
monica-gatti <gatti_monica@libero.it>
2023-01-17 10:11:45 +0100

    SQLAlchemy model

---
+++ src/model.py
@@ -0,0 +1,37 @@
+"""Declare models and relationships."""
+from sqlalchemy import Column, DateTime, Integer, String, Text, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+from sqlalchemy import create_engine
+from Crypto import Random
+from Crypto.Cipher import AES
+
+engine = create_engine("postgresql://postgres:Ev3ryD%40ta@localhost:5432/NyTimes")
+Base = declarative_base()
+class Article(Base):
+    """Article."""
+    __tablename__ = "Article"
+    slug_id = Column(String(255), primary_key=True)
+    article_date = Column(DateTime, unique=False, nullable=False)
+    title = Column(String(255), nullable=False)
+    section = Column(String(255), unique=False, nullable=False)
+    subsection = Column(String(255))
+    url = Column(String(5000))
+    webPageAvailability = Column(String(1))
+    apiInvokeDate = Column(DateTime)
+ 
+    def __repr__(self):
+        return "<articles %r>" % self.title
+
+class Author(Base):
+    """Author."""
+    __tablename__ = "Author"
+    id = Column(Integer, primary_key = True, autoincrement=True)
+    slug_id = Column(String(255), ForeignKey("Article.slug_id"), nullable=False)
+    fullname = Column(String(255), nullable=False)
+ 
+    def __repr__(self):
+        return "<author %r>" % self.fullname
+
+
+Base.metadata.create_all(engine)
